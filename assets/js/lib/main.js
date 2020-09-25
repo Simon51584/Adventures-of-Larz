6bc43e8a6f826f3ee9c434e462144b247fffb735
Simon51584 <simon51584@gmail.com>
2020-09-25 00:40:47 -0500

    Avatar movement established

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -7,6 +7,7 @@ let config = {
     scene: {
         preload: preload,
         create: create,
+        update: update,
     },
     physics: {
         default: "arcade",
@@ -26,6 +27,7 @@ function preload() {
     // this method only loads a single image to the Game 
     // this.load.image("items", "assets/images/items.png")
     this.load.spritesheet("items", "assets/images/items.png", { frameWidth: 32, frameHeight: 32 })
+    this.load.spritesheet("characters", "assets/images/characters.png", { frameWidth: 32, frameHeight: 32 })
 
 }
 
@@ -38,7 +40,31 @@ function create () {
     this.add.sprite(300,100, "button1")
     // button.setOrigin(0, 0)
 
-    this.add.image(300,300, "items", 1)
+    this.add.image(300,300, "items", 0)
 
     this.physics.add.image(500, 100, "button1")
+
+    this.player = this.physics.add.image(32, 32, "characters", 5)
+    this.player.setScale(2)
+
+    this.cursors = this.input.keyboard.createCursorKeys()
+    //This will allow Phaser to detect keyboard inputs
+}
+
+function update () {
+    this.player.setVelocity(0)
+    if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-160)
+    } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(160)
+    }
+    
+    if (this.cursors.up.isDown) {
+    this.player.setVelocityY(-160)
+    
+    } else if (this.cursors.down.isDown) {
+    this.player.setVelocityY(160)
+
+    }
+
 }
