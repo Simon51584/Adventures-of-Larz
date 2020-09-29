2f58716a44357a3513d6bc5a3684e61202ac29b0
Simon51584 <simon51584@gmail.com>
2020-09-29 13:30:27 -0500

    created new chest spawn points as well as well as score tracking

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -5,7 +5,8 @@ class GameScene extends Phaser.Scene {
     }
 
     init() {
-        this.scene.launch("Ui")
+        this.scene.launch("Ui");
+        this.score = 0;
     }
 
     create() {
@@ -33,7 +34,22 @@ class GameScene extends Phaser.Scene {
 
     createChests() {
         // create a chest group
-        this.chest = new Chest(this, 300,300, "items", 0);
+        this.chests = this.physics.add.group();
+        // create chest positions array
+        this.chestPositions = [[100, 100], [200, 200], [300, 300], [400, 400], [500, 500]];
+        // specify the max number of chests we can have
+        this.maxNumberOfChests = 3;
+        for (let i = 0; i < this.maxNumberOfChests; i += 1) {
+            this.spawnChest();
+
+        }
+    }
+
+    spawnChest() {
+        const location = this.chestPositions[Math.floor(Math.random() * this.chestPositions.length)]
+        const chest = new Chest(this, location[0], location[1], "items", 0);
+        // add chest to chests group
+        this.chests.add(chest);
     }
     
     createWalls() {
@@ -47,15 +63,19 @@ class GameScene extends Phaser.Scene {
 
     addCollisions() {
         this.physics.add.collider(this.player, this.wall);
-        this.physics.add.overlap(this.player, this.chest, this.collectChest, null, this);
+        this.physics.add.overlap(this.player, this.chests, this.collectChest, null, this);
     }
 
     collectChest(player, chest){
         // play gold pickup sound
         this.goldPickupAudio.play();
+        // update our score
+        this.score += chest.coins;
         // update score in the ui 
-        this.events.emit("updateScore", chest.coins);
+        this.events.emit("updateScore", this.score);
         // destroy the chest game object
         chest.destroy()
+        // spawn a new chest
+        this.time.delayedCall(1000, this.spawnChest(), [], this);
     }
 }
