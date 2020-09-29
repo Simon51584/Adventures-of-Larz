db41dc0bc0f0cee0f4adca0c16a9d1d1cd3a1104
Simon51584 <simon51584@gmail.com>
2020-09-29 15:25:04 -0500

    implemented tiled map

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -10,9 +10,10 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
+        this.createMap();
         this.createAudio();
         this.createChests();
-        this.createWalls();
+        // this.createWalls();
         this.createPlayer();
         this.addCollisions();
         this.createInput();
@@ -47,15 +48,24 @@ class GameScene extends Phaser.Scene {
 
     spawnChest() {
         const location = this.chestPositions[Math.floor(Math.random() * this.chestPositions.length)]
-        const chest = new Chest(this, location[0], location[1], "items", 0);
-        // add chest to chests group
-        this.chests.add(chest);
+        
+        let chest = this.chests.getFirstDead();
+        console.log(chest);
+        if (!chest) {
+            const chest = new Chest(this, location[0], location[1], "items", 0);
+            // add chest to chests group
+            this.chests.add(chest);
+        
+        } else {
+            chest.setPosition(location[0], location[1]);
+            chest.makeActive();
+        }
     }
     
-    createWalls() {
-        this.wall = this.physics.add.image(500, 100, "button1");
-        this.wall.setImmovable();         
-    }
+    // createWalls() {
+    //     this.wall = this.physics.add.image(500, 100, "button1");
+    //     this.wall.setImmovable();         
+    // }
 
     createInput() {
         this.cursors = this.input.keyboard.createCursorKeys()
@@ -73,9 +83,21 @@ class GameScene extends Phaser.Scene {
         this.score += chest.coins;
         // update score in the ui 
         this.events.emit("updateScore", this.score);
-        // destroy the chest game object
-        chest.destroy()
+        // make chest game objectinactive
+        chest.makeInactive();
         // spawn a new chest
         this.time.delayedCall(1000, this.spawnChest(), [], this);
+    }
+    createMap() {
+        // create the tilemap
+        this.map = this.make.tilemap({ key: "map" });
+        // add the tileset image to our map
+        this.tiles = this.map.addTilesetImage("background", "background", 32, 32, 1, 2);
+        // create our background layer
+        this.backgroundLayer = this.map.createStaticLayer("background", this.tiles, 0, 0);
+        this.backgroundLayer.setScale(2);
+        // create blocked layer
+        this.blockedLayer = this.map.createStaticLayer("blocked", this.tiles, 0 , 0);
+        this.blockedLayer.setScale(2);
     }
 }
